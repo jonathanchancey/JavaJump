68402a2725625750ef9366e69681a056967de66b
jonstar7 <coruscar@Sadie.attlocal.net>
2018-05-02 15:07:31 -0700

    tweaked gravity added failure state
    and the beginning of a enemy class

---
+++ sas/Enemy.h
@@ -0,0 +1,44 @@
+#ifndef Enemy_h
+#define Enemy_h
+
+#include "libs.h"
+
+class Enemy {
+public:
+    float x;
+    float y;
+    float w;
+    float h;
+    GLuint texture_id;
+
+
+    Enemy (const char*, float, float, float, float);
+    
+    void draw();
+    
+    bool contains(float, float);
+    
+    void moveUp(float rate=0.01);
+    void moveDown(float rate=0.01);
+    void moveLeft(float rate=0.01);
+    void moveRight(float rate=0.01);
+    
+    void jump();
+    void java();
+    void activate();
+    
+    bool rising;
+    bool movingLeft;
+   
+    float xinc;
+    float yinc;
+    
+    
+    float velY;
+    float gravity;
+    
+    int jumpAmount;
+    int jumps;
+};
+
+#endif
