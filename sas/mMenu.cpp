c2cde29854ca73dd2ea59a7c8bbfb0aec42c1f89
coruscar <jonathan22711@gmail.com>
2018-05-06 23:18:31 -0700

    init commit

--- sas/mMenu.cpp
+++ sas/mMenu.cpp
@@ -1,7 +1,140 @@
+#include "mMenu.h"
 
-// #include "mMenu.h"
+static mMenu* singleton;
 
-// mMenu::mMenu()
-// {
-//     main_menu = new TexRect("images/main_menu.png", -1, 1, 2, 2);
-// }
+void app_timer(int value){
+    
+    if (singleton->moving){
+        
+        if (singleton->ball->y - singleton->ball->h < -0.99){
+            singleton->moving = false;
+            singleton->game_over = true;
+            singleton->gameOver->animate();
+        }
+    }
+    
+    if (singleton->game_over){
+        singleton->redraw();
+        glutTimerFunc(100, app_timer, value);
+    }
+    else{
+        if (singleton->up || singleton->down || singleton->left || singleton->right || singleton->moving && !singleton->game_over){
+            singleton->redraw();
+            glutTimerFunc(16, app_timer, value);
+        }
+    }
+    
+    
+}
+
+mMenu::mMenu(const char* label, int x, int y, int w, int h): GlutApp(label, x, y, w, h){
+    // Initialize state variables
+    
+    singleton = this;
+    mx = 0.0;
+    my = 0.0;
+    
+    main_menu = new TexRect("images/main_menu.png", -1, 1, 2, 2);
+    moving = true;
+    game_over = false;
+    
+    app_timer(1);
+    
+}
+
+void mMenu::specialKeyPress(int key){
+    if (!game_over){
+        if (key == 100){
+            left = true;
+        }
+        if (key == 101){
+            //up = true;
+        }
+        if (key == 102){
+            right = true;
+        }
+        if (key == 103){
+            //down = true;
+        }
+    }
+}
+
+void mMenu::specialKeyUp(int key){
+    if (key == 100) {
+        left = false;
+    }
+    if (key == 101) {
+        up = false;
+    }
+    if (key == 102) {
+        right = false;
+    }
+    if (key == 103) {
+        down = false;
+    }
+}
+
+void mMenu::draw() {
+    
+    // Clear the screen
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // Set background color to black
+    glClearColor(0.0, 0.0, 1.0, 1.0);
+    
+    // Set up the transformations stack
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    
+    background->draw();
+    platform->draw();
+    ball->draw();
+    gameOver->draw();
+    
+    // We have been drawing everything to the back buffer
+    // Swap the buffers to see the result of what we drew
+    glFlush();
+    glutSwapBuffers();
+}
+//
+//void mMenu::mouseDown(float x, float y){
+//    // Update app state
+//    mx = x;
+//    my = y;
+//
+//}
+//
+//void mMenu::mouseDrag(float x, float y){
+//    // Update app state
+//    mx = x;
+//    my = y;
+//
+//}
+
+void mMenu::idle(){
+    
+}
+
+void mMenu::keyPress(unsigned char key) {
+    if (key == 27){
+        // Exit the app when Esc key is pressed
+        delete main_menu;
+        delete this;
+        
+        exit(0);
+    }
+    
+    if (key == ' '){
+//        ball->x = 0;
+//        ball->y = 0.67;
+//        ball->yinc = 0.01;
+//        ball->xinc = 0.01;
+//        ball->rising = false;
+        game_over = false;
+//        gameOver->stop();
+        moving = true;
+    }
+}
+
+
+
--- sas/mMenu.h
+++ sas/mMenu.h
@@ -1,22 +1,53 @@
 
 
-// #ifndef mMenu_hpp
-// #define mMenu_hpp
+#ifndef mMenu_hpp
+#define mMenu_hpp
 
-// #include <stdio.h>
-// #include "TexRect.h"
-// #include <iostream>
-// #include "GlutApp.h"
+#include <stdio.h>
+#include "TexRect.h"
+#include <iostream>
+#include "GlutApp.h"
+//#include "libs.h"
 
-// class mMenu: public GlutApp {
-//     public:
+//#include "GlutApp.h"
+//#include "TexRect.h"
+//#include "AnimatedRect.h"
+
+class mMenu: public GlutApp {
+    // Maintain app state here
+    float mx;
+    float my;
+public:
+    // Constructor, to initialize state
+    mMenu(const char* label, int x, int y, int w, int h);
     
-//         mMenu();
-//         TexRect* main_menu;
+    // These are the events we want to handle
+    void draw();
+    void keyPress(unsigned char key);
+    void mouseDown(float x, float y);
+    void mouseDrag(float x, float y);
+    
+    void specialKeyPress(int key);
+    
+    void specialKeyUp(int key);
+    
+    void idle();
+    
+    TexRect* main_menu;
 
-// };
+    
+    bool up;
+    bool down;
+    bool left;
+    bool right;
+    
+    bool moving;
+    bool game_over;
+    
+    
+};
 
 
+#endif /* mMenu_hpp */
 
 
-// #endif /* mMenu_hpp */
