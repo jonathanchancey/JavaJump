6030751ffbd3f7ad4e7781704293a29d69579e45
AlecMendiola <mendiolaalec@gmail.com>
2018-05-07 22:55:21 -0700

    added highScore class & score txt

---
+++ sas/highScore.cpp
@@ -0,0 +1,49 @@
+// #include <highScore.h>
+// #include <iostream>
+// #include <fstream>
+// #include <sstream>
+// using namespace std;
+//
+// ofstream scoreFile;
+//
+//
+// highScore::highScore(int x, int y){
+//
+//
+//
+//
+// }
+
+
+// void highScore::addEneScore(){
+//
+// }
+
+// bool highScore::checkScore(int userScore){
+//   string strLine;
+// scoreFile.open("scoreList.txt");
+// if(scoreFile.is_open()){
+//   while(getline(scoreFile,strLine)){
+//
+//   }
+//   scoreFile.close();
+// }
+//
+// }
+
+// if(score > ){
+//   return true;
+// }
+//
+// else{
+//   return false;
+// // }
+// }
+
+// void highScore::storeScore(int score){
+//   //"scoreList.txt"
+//
+//   scoreFile.open("scoreList.txt");
+//   scoreFile << score;
+//   scoreFile.close();
+// }
